62f5c67c49260c106b02ad28e28f00a9295ab0c8
Ethan-Software-Developer <ethansevenster5@gmail.com>
2025-01-05 13:59:50 -0800

    c

--- university/university/settings.py
+++ university/university/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-ylp%kghf_l7itlrp2!04u5_)rsjj5_#!y(mx3vb((te&mth1(0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
